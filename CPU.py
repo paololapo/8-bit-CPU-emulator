8c2bde0da93bb90bd3e5f17f57ee1d15db9e264b
Lorenzo Vigorelli <131964372+LorenzoVigorelli@users.noreply.github.com>
2024-01-09 22:42:13 +0100

    Update CPU.py

--- CPU.py
+++ CPU.py
@@ -162,19 +162,24 @@ class CPU:
                         
                     if len(parts) == 2:
                         instruction = parts[0]
-                        hex_number = parts[1]
-                        # Convert instruction to op code
-                        op_code = self.convert_mnemo_op_code(instruction)
-                        # Extract the hexadecimal number from the target line
-                        binary_number = hex_to_binary(hex_number)
-                        # Combine op code and binary number
-                        combined_data = op_code + binary_number
+	                hex_number = parts[1]
+	                # Convert instruction to op code
+	                op_code = convert_mnemo_op_code(instruction)
+	                if hex_number == "0x0":
+	                	binary_number = "0000"
+	        	else:
+	                	# Extract the hexadecimal number from the target line
+	                	binary_number = hex_to_binary(hex_number)
+	                # Combine op code and binary number
+	                combined_data = op_code + binary_number
                     
                     
                 elif not line[0].isalpha():
                     # Split the line into instruction and hexadecimal number
-                    combined_data = hex_to_binary(line).zfill(8)
-                
+                	if line == "0x0":
+                        	combined_data = "00000000"
+                	else:
+                    		combined_data = hex_to_binary(line).zfill(8
                 else:
                     continue
                 
